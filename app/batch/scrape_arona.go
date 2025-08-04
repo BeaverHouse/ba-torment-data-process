df31b93871fb65e199c17990035325b3b08c27e7
Austin Lee <haulrest@gmail.com>
2025-08-04 15:13:37 +0900

    Fix to get by season number (total assault)

--- app/batch/scrape_arona.go
+++ app/batch/scrape_arona.go
@@ -10,22 +10,27 @@ import (
 
 func ScrapeAronaAI() {
 	log.Println("Starting to scrape data from arona.ai")
-	data, err := scrape.GetDataFromAronaAI()
-	if err != nil {
-		log.Fatalf("Failed to get data from arona.ai: %v", err)
-	}
 
-	// For now, we'll just print the first 100 characters of the data.
-	// In a real scenario, you might want to save it to a file or process it further.
-	if len(data) > 100 {
-		fmt.Println(data[:100])
-	} else {
-		fmt.Println(data)
-	}
+	seasons := []string{"S79-0", "S78-0"}
 
-	// save tValue to file
-	json.Marshal(data)
-	os.WriteFile("data/S79-0.json", []byte(data), 0644)
+	for _, season := range seasons {
+		data, err := scrape.GetDataFromAronaAI(season)
+		if err != nil {
+			log.Fatalf("Failed to get data from arona.ai: %v", err)
+		}
+
+		// For now, we'll just print the first 100 characters of the data.
+		// In a real scenario, you might want to save it to a file or process it further.
+		if len(data) > 100 {
+			fmt.Println(data[:100])
+		} else {
+			fmt.Println(data)
+		}
+
+		// save tValue to file
+		json.Marshal(data)
+		os.WriteFile("data/"+season+".json", []byte(data), 0644)
+	}
 
 	log.Println("Successfully scraped data.")
 }
--- scrape/arona_ai.go
+++ scrape/arona_ai.go
@@ -1,16 +1,23 @@
 package scrape
 
 import (
+	"ba-torment-data-process/app/logic"
 	"context"
 	"log"
+	"runtime"
+	"strconv"
 	"time"
 
 	"github.com/chromedp/chromedp"
 )
 
-func GetDataFromAronaAI() (string, error) {
+func GetDataFromAronaAI(seasonString string) (string, error) {
+	path := "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
+	if runtime.GOOS == "darwin" {
+		path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	}
 	parentContext, parentContextCancel := chromedp.NewExecAllocator(context.Background(),
-		chromedp.ExecPath(`C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe`),
+		chromedp.ExecPath(path),
 	)
 	defer parentContextCancel()
 
@@ -22,11 +29,30 @@ func GetDataFromAronaAI() (string, error) {
 	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 
-	var tValue string
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(`https://arona.ai/raidreport`),
+	season, category, err := logic.SplitSeasonString(seasonString)
+	if err != nil {
+		return "", err
+	}
+	seasonNum, err := strconv.Atoi(season[1:])
+	if err != nil {
+		return "", err
+	}
 
-		// 1. "직접 검색" 버튼이 보일 때까지 스크롤하고 클릭합니다.
+	url := "https://arona.ai/raidreport"
+	if category != 0 {
+		url = "https://arona.ai/eraidreport"
+	}
+
+	var tValue string
+	err = chromedp.Run(ctx,
+		chromedp.Navigate(url),
+
+		// 1. 알맞은 시즌을 선택합니다. (seasonNum + . 으로 시작하는 div 클릭 (ex. 79. 으로 시작하는))
+		chromedp.WaitVisible(`//div[@role="combobox"]`),
+		chromedp.Click(`//div[@role="combobox"]`, chromedp.NodeVisible),
+		chromedp.Click(`//li[@data-value="`+strconv.Itoa(seasonNum)+`"]`, chromedp.NodeVisible),
+
+		// 2. "직접 검색" 버튼이 보일 때까지 스크롤하고 클릭합니다.
 		chromedp.WaitVisible(`//div[text()="직접 검색"]`),
 		chromedp.ScrollIntoView(`//div[text()="직접 검색"]`),
 		chromedp.Click(`//div[text()="직접 검색"]`, chromedp.NodeVisible),
@@ -42,25 +68,25 @@ func GetDataFromAronaAI() (string, error) {
 			};
 		})();`, nil),
 
-		// 2. role="combobox"인 5번째 div를 클릭합니다.
+		// 3. role="combobox"인 5번째 div를 클릭합니다.
 		chromedp.WaitVisible(`(//div[@role="combobox"])[5]`),
 		chromedp.Click(`(//div[@role="combobox"])[5]`, chromedp.NodeVisible),
 
-		// 3. data-value="20000"인 li를 클릭합니다.
+		// 4. data-value="20000"인 li를 클릭합니다.
 		chromedp.WaitVisible(`//li[@data-value="20000"]`),
 		chromedp.Click(`//li[@data-value="20000"]`, chromedp.NodeVisible),
 
-		// 4. 데이터가 로드되기를 기다립니다.
+		// 5. 데이터가 로드되기를 기다립니다.
 		chromedp.Sleep(3*time.Second),
 
-		// 5. 주입된 JavaScript를 통해 전역 변수에 저장된 't' 값 추출
+		// 6. 주입된 JavaScript를 통해 전역 변수에 저장된 't' 값 추출
 		chromedp.Evaluate(`window.aronaDataT`, &tValue),
 	)
 	if err != nil {
 		return "", err
 	}
 
-	log.Printf("Successfully retrieved 't' variable: %s", tValue)
+	log.Printf("Successfully retrieved data from arona.ai")
 
 	return tValue, nil
 }
