94ddacfb9794214d204ef247368295a8683de2e3
Austin Lee <haulrest@gmail.com>
2025-12-11 00:04:20 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 tag=v0.1.1
 
-docker build -t ghcr.io/BeaverHouse/ba-data-process:$tag .
-docker push ghcr.io/BeaverHouse/ba-data-process:$tag
+docker build -t ghcr.io/beaverhouse/ba-data-process:$tag .
+docker push ghcr.io/beaverhouse/ba-data-process:$tag
