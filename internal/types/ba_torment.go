fe63030ce18b213f7f77b3ea52eccc050f457b08
Austin Lee <haulrest@gmail.com>
2026-07-04 18:46:56 +0900

    Remove skill orders from analysis type

--- internal/types/ba_torment.go
+++ internal/types/ba_torment.go
@@ -94,7 +94,6 @@ type VideoAnalysisListResponse struct {
 
 // YoutubeAnalysisResult represents the analysis_result JSONB field in youtube_analysis table
 type YoutubeAnalysisResult struct {
-	Score       int      `json:"score"`
-	PartyData   [][6]int `json:"partyData"`
-	SkillOrders []any    `json:"skillOrders"`
+	Score     int      `json:"score"`
+	PartyData [][6]int `json:"partyData"`
 }
